24d54871d6b063242ec00d1983802617719199be
hibby <d@vehibberd.com>
2018-10-14 14:21:23 +0000

    Unlocking the door now broadcasts on mqtt!

--- unlock.py
+++ unlock.py
@@ -2,12 +2,33 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
+import paho.mqtt.client as mqtt
+# import the secrets from config.py, not committed to the repo
+import config
+# Config's layout looks like this:
+#MQTT_CONFIG = {
+#    'host': 'aaa.bbb.ccc.ddd',
+#    'user': 'username',
+#    'pass': 'pasword'
+#}
+
+#set up mqtt optoins
+mqttc = mqtt.Client()
+mqttc.username_pw_set(config.MQTT_CONFIG['user'], config.MQTT_CONFIG['pass'])
 
 pin = 26
 
+# Set up pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
+# Unlock Door
 GPIO.output(pin, 1)
+# connect to mqtt
+mqttc.connect(config.MQTT_CONFIG['host'])
+# publish to space/status topic, unlocked will trigger the relevant script in
+# hass
+infot = mqttc.publish("space/status", "unlocked")
+mqttc.disconnect()
 sleep(10)
+# Lock Door
 GPIO.output(pin, 0)
-
